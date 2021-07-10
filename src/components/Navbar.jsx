0b03ec6e7a56dd1455be81f8de31ebf1fdb23252
BDWNAV <rs736611@gmail.com>
2021-07-10 20:39:59 +1000

    Made some changes.

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Navbar extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Ur mum kinda sus loki ngl</h1>
+            </div>
+        )
+    }
+}
