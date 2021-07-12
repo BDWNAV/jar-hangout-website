bd738b97ba8397df363a956a9dd3fd6b916f0114
BDWNAV <rs736611@gmail.com>
2021-07-12 18:51:05 +1000

    Made some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import "./css/Header.css";
 
 export default class App extends React.Component {
   render() {
     return (
       <main>
-        <Navbar />
         <Switch>
           <Route path="/" component={Home} />
         </Switch>
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+  render() {
+    return(
+      <div className="div">
+        <h1>Hello.</h1>
+      </div>
+    );
+  }
+}
