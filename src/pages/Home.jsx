0fd8dfcce91aed883da743ee29c21f6fc40665f4
BDWNAV <rs736611@gmail.com>
2021-07-13 09:08:15 +0000

    Added some text to the main page.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,9 @@ export default class Home extends React.Component {
     return (
       <div>
         <Header />
+        <div className="content">
+          <p>What is JAR hangout? Well JAR hangout is a awesome community with a bunch of cool and chill people, so if this interests you please feel free to join the following <a href="https://jarhangout.ml/">link</a></p>
+        </div>
       </div>
     );
   }
