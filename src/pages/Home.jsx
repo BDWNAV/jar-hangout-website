f9348c1d547aeb153496d1102366849660452c8b
Navraaj <rs736611@gmail.com>
2021-07-14 17:59:32 +1000

    Added some new content to the main pages see changes at https://jarhangout.ml/

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ export default class Home extends React.Component {
       <div>
         <Header />
         <div className="content">
-          <p>What is JAR hangout? Well JAR hangout is a awesome community with a bunch of cool and chill people, so if this interests you please feel free to join the following <a href="https://jarhangout.ml/">link</a></p>
+          <p>What is JAR hangout? Well JAR hangout is a awesome community with a bunch of cool and chill people, so if this interests you please feel free to join the following <a href="https://jarhangout.ml/">link</a>. We will be shortly be accepting new staff applications.</p>
         </div>
       </div>
     );
