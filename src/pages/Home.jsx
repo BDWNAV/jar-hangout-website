ad88e76d668ce04ec28866abeb24cf1ba9fb3fcf
BDWNAV <rs736611@gmail.com>
2021-07-20 15:31:58 +1000

    Made some cahnges to the website.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ export default class Home extends React.Component {
       <div>
         <Header />
         <div className="content">
-          <p>What is JAR hangout? Well JAR hangout is a awesome community with a bunch of cool and chill people, so if this interests you please feel free to join the following <a href="https://jarhangout.ml/">link</a>. We will be shortly be accepting new staff applications.</p>
+          <p>Wondering why and what JAR hangout is? Well JAR hangout is a awesome place to vibe with a bunch of cool and chill people! Still not convinced? Well we are currently working on custom bot! The bot will have currency but for a bit, you guys will have to use dank memer. We will be acceping staff applications shortly!</p>
         </div>
       </div>
     );
