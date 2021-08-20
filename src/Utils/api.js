915f4be4a47bdf4e4a67896b684233a689584843
BDWNAV <rs736611@gmail.com>
2021-08-20 21:16:48 +1000

    Fixed the api successfully

--- src/Utils/api.js
+++ src/Utils/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export function getUser() {
-  axios.get("http://91.109.116.52:4055/api/auth", { withCredentials: true });
+  return axios.get("http://91.109.116.52:4055/api/auth/", { withCredentials: true })
 }
