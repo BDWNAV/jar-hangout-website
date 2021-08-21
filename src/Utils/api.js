def4da67329df6c310c4a9ca9014a8f16b40a8d8
BDWNAV <rs736611@gmail.com>
2021-08-21 20:39:10 +1000

    Fixed the api finally.

--- src/Utils/api.js
+++ src/Utils/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export function getUser() {
-  return axios.get("http://91.109.116.52:4055/api/auth/", { withCredentials: true })
+  return axios.get("https://api.jarhangout.ml/api/auth/", { withCredentials: true })
 }
