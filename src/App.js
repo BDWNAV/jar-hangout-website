c1eb1191e5efb4a631959b43f23ab5c3425918e1
BDWNAV <rs736611@gmail.com>
2021-09-27 20:30:24 +1000

    Changed some file names

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Staff from "./pages/Staff";
+import StaffDashboard from "./pages/StaffDashboard";
+import Suggestion from "./pages/Suggestion";
+import "./css/App.css";
+
+export default function App() {
+  return (
+    <main>
+      <Switch>
+        <Route path="/suggestions/create" component={Suggestion} />
+        <Route path="/dash/staff/:userId" component={StaffDashboard} />
+        <Route path="/staff" component={Staff} />
+        <Route path="/" component={Home} />
+      </Switch> 
+    </main>
+  );
+}
